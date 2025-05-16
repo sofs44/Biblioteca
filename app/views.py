97a60d85432b1f986aba1ef8917cd08ccb52fa6a
sofs44 <sofia.sc.080207@gmail.com>
2025-05-16 12:23:49 -0300

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from django.views import View
+from django.contrib import messages
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+    def post(self, request):
+        pass
