bad5af28b2652dd17a7c25da3930e83f9a28b0ec
sofs44 <sofia.sc.080207@gmail.com>
2025-05-19 11:54:51 -0300

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5', 
     'app',
 ]
 
@@ -95,11 +97,6 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
- INSTALLED_APPS = [
-    ...
-    'crispy_forms',
-    'crispy_bootstrap5',  # se estiver usando Bootstrap 5
-]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
